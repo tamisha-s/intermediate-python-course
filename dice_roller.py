405b8f63432c7ccf911b18b5ddce4b799fa1dc64
Dzifa <tamisha.segbefia@ashesi.edu.gh>
2020-08-04 16:45:37 +0000

    - added player names - stored player totals in an array - determined the winner from the highest score in the array

--- dice_roller.py
+++ dice_roller.py
@@ -5,22 +5,35 @@ import random
 
 
 def main():
-    dice_rolls = int(input('How many dice would you like to roll? '))
+    print(
+        "Thank you for coming to this game! This is just a simple dice roller game that compares your roll sum to other roll sums.\n"
+        "The winner is determined by the team that has the highest roll. There can be multiple winners.\n"
+        "Whatever the case, we hope you have an amazing time!\n")
+    number_of_teams = int(input('How many teams or people are there? '))
+
+    player_scores = []
+    player_names = []
+    for i in range(0, number_of_teams):
+        player_scores.append(0)
+        user_names = input('Please enter your name or your team name: ')
+        print(f'Welcome, {user_names}! Your position in the team array is {i}.\n')
+        player_names.append(user_names)
+
+    dice_rolls = int(input('How many dice would you like to roll per team? '))
     dice_size = int(input('How many sides has the dice? '))
-    dice_sum = 0
 
-    for i in range(0, dice_rolls):
-        roll = random.randint(1, dice_size)
-        dice_sum += roll
+    for i in range(0, number_of_teams):
+        dice_sum = 0
+        for j in range(0, dice_rolls):
+            roll = random.randint(1, dice_size)
+            dice_sum += roll
+        player_scores[i] = dice_sum
 
-        if roll == 1:
-            print(f'You rolled a {roll}! Critical Fail')
-        elif roll == dice_size:
-            print(f'You rolled a {roll}! Critical Success!')
-        else:
-            print(f'You rolled a {roll}')
+    highest_score = max(player_scores)
 
-    print(f'You have rolled a total of {dice_sum}')
+    for i in range(0, number_of_teams):
+        if player_scores[i] == highest_score:
+            print(f'\nCongratulations, {player_names[i]}! You win this round with an amazing score of {highest_score}!')
 
 
 if __name__ == "__main__":
